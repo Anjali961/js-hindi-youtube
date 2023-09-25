24a6fc6d7380f406212f43e4b8389a8303496e16
Roshan Maurya <roshanmaurya456@gmail.com>
2023-09-25 10:15:09 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc"; //print number as datatype
+let score1 = null; //print 0 on datatype check
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //number return karega
+// console.log(valueInNumber); //NaN return karega
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1; // iska output true dega
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true dega // 0=> false dega
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33;
+
+let stringNumber =  String(someNumber);
+
+console.log(stringNumber); //this will return number 33
+
+
+console.log(typeof someNumber); //this will print number as number
