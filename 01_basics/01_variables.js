6702c8ba25e84ec2732e3edb099d8a5dbcaf2937
Roshan Maurya <roshanmaurya456@gmail.com>
2023-10-09 08:55:29 +0000

    anjali ka naam

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -18,3 +18,7 @@ prefer not to use var because of issue in block scope and functional scope
  */
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+//anjali
